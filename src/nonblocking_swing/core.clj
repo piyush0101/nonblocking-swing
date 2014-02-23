3fb3b2bc85e9ebe40ab284d3431c85389a44df36
Piyush Srivastava <piyush.0101@gmail.com>
2014-02-23 12:51:37 -0600

    adding a button that blocks the UI

---
+++ src/nonblocking_swing/core.clj
@@ -0,0 +1,36 @@
+(ns nonblocking-swing.core)
+
+(import javax.swing.JFrame)
+(import javax.swing.JButton)
+(import java.awt.GridLayout)
+(import '(java.awt.event ActionListener ActionEvent))
+
+(def frame (JFrame. "Frame"))
+
+(defn create-long-running-task-button
+  []
+  (let [button (JButton. "LongRunningTask")]
+    (.addActionListener
+       button
+       (proxy [ActionListener] []
+         (actionPerformed [event]
+                          (Thread/sleep 5000))))
+    button))
+
+(defn add-buttons
+  []
+  (let [layout (GridLayout. 3 1)]
+    (.setLayout frame layout)
+    (.add frame (create-long-running-task-button))
+    (.add frame (JButton. "Always Clickable"))))
+
+(defn create-frame
+  []
+  (.setSize frame 300 300)
+  (.setVisible frame true)
+  (add-buttons))
+
+
+(create-frame)
+
+
---
+++ test/nonblocking_swing/core_test.clj
@@ -0,0 +1,7 @@
+(ns nonblocking-swing.core-test
+  (:use clojure.test
+        nonblocking-swing.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
